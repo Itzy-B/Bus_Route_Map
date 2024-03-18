f6253a268bd22c2ec42c8d976c21305fbc31ed40
joelkumi <joelkumi@protonmail.com>
2024-03-18 10:06:16 +0100

    Refactored Packages

--- src/java/Main/Main.java
+++ src/java/Main/Main.java
@@ -1,9 +1,10 @@
 package src.java.Main;
 //Main Will Be Instancing of GUI
 
+import static src.java.Main.CalculateDistance.getDistance;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getDistance("6222CN", "6213HD"));
     }
 }
