776c6ea08e08cb5b74dac7e69f467cab9d5bb27b
joelkumi <joelkumi@protonmail.com>
2024-06-10 12:41:16 +0200

    Modified the JSON controller to use an API call to find the Postal Code of the school as well as the name if it is missing from the geojson file

--- src/java/JSON/JSONController.java
+++ src/java/JSON/JSONController.java
@@ -5,12 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import src.java.GUI.Data;
 
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JSONController {
+    private static final String API_URL_TEMPLATE = "https://geocode.maps.co/reverse?lat=%f&lon=%f&api_key=6666d2ba4331d378476246ngzcb1fcc";
 
     private static final String SCHOOL_AMENITY = "school";
 
@@ -39,9 +44,51 @@ public class JSONController {
                     ", longitude=" + longitude +
                     '}';
         }
+
     }
 
-    public List<School> getSchoolsFromGeoJSON(String filePath) throws IOException {
+    public static String[] getPostalCode(double latitude, double longitude) {
+        String[] data = new String[2];
+        String apiUrl = String.format(API_URL_TEMPLATE, latitude, longitude);
+        try {
+            URL url = new URL(apiUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            StringBuilder response = new StringBuilder();
+            String output;
+            while ((output = br.readLine()) != null) {
+                response.append(output);
+            }
+
+            conn.disconnect();
+
+            // Parse JSON response
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response.toString());
+            JsonNode addressNode = rootNode.get("address");
+            JsonNode nameNode = rootNode.get("display_name");
+            if (addressNode != null) {
+                JsonNode postcodeNode = addressNode.get("postcode");
+                if (postcodeNode != null) {
+                    data[0] = postcodeNode.asText();
+                    data[1] = nameNode.asText().split(", ")[0];
+                    return data;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<School> getSchoolsFromGeoJSON(String filePath) throws IOException, InterruptedException {
         Data.getData();
         List<School> schools = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
@@ -61,8 +108,13 @@ public class JSONController {
                             if (coordinates != null && coordinates.isArray() && coordinates.size() == 2) {
                                 double longitude = coordinates.get(0).asDouble();
                                 double latitude = coordinates.get(1).asDouble();
-                                String postcode = Data.findClosestZipCode(latitude, longitude);
+                                String[] data = getPostalCode(latitude, longitude);
+                                String postcode = data[0];
+                                if(name.equals("Unnamed School")){
+                                    name = data[1];
+                                }
                                 schools.add(new School(name, postcode, latitude, longitude));
+                                Thread.sleep(1000);
                             }
                         }
                     }
@@ -81,6 +133,8 @@ public class JSONController {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
