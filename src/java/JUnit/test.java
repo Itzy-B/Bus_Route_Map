5b8dd8bebb2f9cd0dcfc2d4a8011d6056bf9c8f0
Tristanko <i6343359@student.maastrichtuniveristy.nl>
2024-06-10 12:02:55 +0200

    Added JUnit testing libs

---
+++ src/java/JUnit/test.java
@@ -0,0 +1,11 @@
+    package src.java.JUnit;
+    import org.junit.Test;
+
+    import static org.junit.Assert.*;  
+
+    public class test {
+        @Test  
+        public void testFindMax(){  
+            assertEquals("null","null");  
+        }  
+    }
