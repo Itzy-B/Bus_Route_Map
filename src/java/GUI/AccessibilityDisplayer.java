4fe9cfe11c2021351661e2a2e49468c35c31415a
Tristanko <62807768+Tristan-Dormans@users.noreply.github.com>
2024-06-13 17:15:17 +0200

    Updated AccessibilityDrawer

--- src/java/GUI/AccessibilityDisplayer.java
+++ src/java/GUI/AccessibilityDisplayer.java
@@ -1,38 +1,38 @@
 package src.java.GUI;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import javafx.event.ActionEvent;
 import javafx.scene.image.Image;
-import src.java.Draw.DrawZipcodes;
-
-import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AccessibilityDisplayer extends JFrame implements ActionListener{
-    private double CENTER_LATITUDE = 50.866825678462234;
-    private double CENTER_LONGITUDE = 5.65494508021394;
-    final int R = 6371;
-    private static final String API_KEY = "AIzaSyDnJH0pu5NzqH0b6GjiPyTDfdkBDugYw6w";
-    private static int scale = 1;
-    private static int zoomLevel = 14;
+    private static final double MAP_WIDTH = 600; // Width of your map image
+    private static final double MAP_HEIGHT = 600;
+    private double CENTER_LATITUDE = 50.851368;
+    private double CENTER_LONGITUDE = 5.690973;
+    private static int zoomLevel = 13;
     private static JButton button;
+    private JPanel panel;
+    static double offset = 268435456; 
+    static double radius = offset / Math.PI;
     private JFrame frame;
-    private List<ImageIcon> images;
 
     public AccessibilityDisplayer() {
         frame = new JFrame();
@@ -51,12 +51,100 @@ public class AccessibilityDisplayer extends JFrame implements ActionListener{
         frame.setVisible(true);
     }
 
-    public void drawPoints() {
+    public void getCircle() throws IOException {
+        Data.getData();
+        ArrayList<String> zipCodes = Data.getZipCodes();
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        frame.getContentPane().removeAll();
+        frame.add(button);
+        frame.add(panel);
+        BufferedImage bufferedImage =  ImageIO.read(new File("./staticmap.png"));
+        Graphics2D g = (Graphics2D) bufferedImage.getGraphics();
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Stroke originalStroke = g.getStroke();
+        g.setStroke(new BasicStroke(5));
+        g.setColor(new Color(255, 0, 0, 64));
+        g.setStroke(originalStroke);
+        ImageIcon imageIcon = null;
+        JLabel label = new JLabel(imageIcon);
+        long startTime = System.currentTimeMillis();
+    
+        for (int index = 0; index < zipCodes.size(); index++) {
+            if (index % 100 == 0) {
+                System.out.println(index);
+                imageIcon = new ImageIcon(bufferedImage);
+                label.setIcon(imageIcon);
+                panel.add(label);
+                frame.setVisible(true); 
+                java.awt.Image image = imageIcon.getImage();
+                bufferedImage = (BufferedImage) image;
+                g = (Graphics2D) bufferedImage.getGraphics();
+                g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                originalStroke = g.getStroke();
+                g.setStroke(new BasicStroke(5));
+                g.setColor(new Color(255, 0, 0, 64));
+                g.setStroke(originalStroke);
+            }
+            Place coordinatesZipCode = new Place(zipCodes.get(index));
+            int[] Xy = adjust(coordinatesZipCode.getLongitude(), coordinatesZipCode.getLatitude(), CENTER_LONGITUDE,CENTER_LATITUDE,zoomLevel);
+            // int[] Xy = adjust(50.83702537886311, 5.69366003053462,CENTER_LATITUDE, CENTER_LONGITUDE,zoomLevel);
+            g.fillOval((int)(Xy[0] + MAP_WIDTH/2 -5), (int) (Xy[1] + MAP_HEIGHT/2 -5), 10, 10);
+        }
 
+        long endTime = System.currentTimeMillis();
+        long duration = (endTime - startTime); // Calculate the duration in milliseconds
+        System.out.println("Execution time in milliseconds: " + duration/1000);
+        
+        imageIcon = new ImageIcon(bufferedImage);
+        label.setIcon(imageIcon);
+        panel.add(label);
+        frame.setVisible(true);
     }
+
+    // X,Y ... location in degrees
+    // xcenter,ycenter ... center of the map in degrees (same value as in 
+    // the google static maps URL)
+    // zoomlevel (same value as in the google static maps URL)
+    // xr, yr and the returned Point ... position of X,Y in pixels relative 
+    // to the center of the bitmap
+
+    //https://stackoverflow.com/questions/23898964/getting-pixel-coordinated-from-google-static-maps
+    public int[] adjust(double X, double Y, double xcenter, double ycenter, int zoomlevel) {
+        int xr = (lToX(X) - lToX(xcenter)) >> (21 - zoomlevel);
+        int yr = (lToY(Y) - lToY(ycenter)) >> (21 - zoomlevel);
+        return new int[]{xr, yr};
+    }
+
+    public static int lToX(double x) {
+        return (int)(Math.round(offset + radius * x * Math.PI / 180));
+    }
+
+    public static int lToY(double y) {
+        return (int)(Math.round(offset - radius * Math.log((1 + Math.sin(y * Math.PI / 180)) / (1 - Math.sin(y * Math.PI / 180))) / 2));
+    }
+    
+    
 
     public static void main(String[] args) {
         AccessibilityDisplayer displayer = new AccessibilityDisplayer();
+        try {
+            displayer.getCircle();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                try {
+                    displayer.getCircle();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+        });
     }
     @Override
     public void actionPerformed(java.awt.event.ActionEvent e) {
--- src/java/GUI/AccessibilityDisplayerOld.java
+++ src/java/GUI/AccessibilityDisplayerOld.java
@@ -1,29 +1,21 @@
 package src.java.GUI;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
-import javafx.event.ActionEvent;
-import javafx.scene.image.Image;
-import src.java.Draw.DrawZipcodes;
-
 import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AccessibilityDisplayerOld extends JFrame implements ActionListener{
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import src.java.Draw.DrawZipcodes;
+
+public class AccessibilityDisplayerOld extends JFrame implements ActionListener {
     private double CENTER_LATITUDE = 50.866825678462234;
     private double CENTER_LONGITUDE = 5.65494508021394;
     final int R = 6371;
@@ -61,7 +53,8 @@ public class AccessibilityDisplayerOld extends JFrame implements ActionListener{
 
     private double calculateLatIncrement(double currentLat, double zoom) {
         double metresAtEquatorPerTilePx = 156543.03392 / Math.pow(2, zoom);
-        return (DEGREES_PER_METER_AT_EQUATOR * Math.cos(currentLat * Math.PI / 180) * metresAtEquatorPerTilePx * TILE_PX) * MULTIPLIER;
+        return (DEGREES_PER_METER_AT_EQUATOR * Math.cos(currentLat * Math.PI / 180) * metresAtEquatorPerTilePx
+                * TILE_PX) * MULTIPLIER;
     }
 
     private double calculateLonIncrement(double zoom) {
@@ -80,7 +73,7 @@ public class AccessibilityDisplayerOld extends JFrame implements ActionListener{
         double lonIncrement = 0;
         double increment = 0.0351;
         List<ImageIcon> images = new ArrayList<ImageIcon>();
-        for (int index = 0; index < 601; index+= 200) {
+        for (int index = 0; index < 601; index += 200) {
             startPos = index;
             if (2783 - index == 183) {
                 index = 2783;
@@ -88,12 +81,13 @@ public class AccessibilityDisplayerOld extends JFrame implements ActionListener{
                 endPosIncrement = 183;
             }
 
-            
-            StringBuilder mapUrlBuilder = createAPIStringBuilderTemplate(CENTER_LATITUDE + latIncrement, CENTER_LONGITUDE + lonIncrement);
+            StringBuilder mapUrlBuilder = createAPIStringBuilderTemplate(CENTER_LATITUDE + latIncrement,
+                    CENTER_LONGITUDE + lonIncrement);
             latIncrement += 0.0351;
             // latIncrement += calculateLatIncrement(CENTER_LATITUDE, zoomLevel);
             // lonIncrement += calculateLonIncrement(zoomLevel);
-            // mapUrlBuilder.append(drawer.drawOnZipCodes(startPos, index + endPosIncrement));
+            // mapUrlBuilder.append(drawer.drawOnZipCodes(startPos, index +
+            // endPosIncrement));
             // mapUrlBuilder.append("&style=feature:all|element:labels|visibility:off");
             mapUrlBuilder.append("&key=").append(API_KEY);
             System.out.println(mapUrlBuilder.toString());
@@ -102,11 +96,11 @@ public class AccessibilityDisplayerOld extends JFrame implements ActionListener{
                 ImageIcon imageIcon = new ImageIcon(url);
                 // JLabel label = new JLabel(imageIcon);
                 // frame.getContentPane().add(label);
-                // BufferedImage bufferedImage = new BufferedImage(imageIcon.getIconWidth(), imageIcon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+                // BufferedImage bufferedImage = new BufferedImage(imageIcon.getIconWidth(),
+                // imageIcon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
                 // bufferedImage.getGraphics().drawImage(imageIcon.getImage(), 0, 0, null);
                 // BufferedImage cropped = bufferedImage.getSubimage(50,50, 300,300);
                 // imageIcon = new ImageIcon(cropped);
-
 
                 System.out.println("added image");
                 JLabel label = new JLabel(imageIcon);
