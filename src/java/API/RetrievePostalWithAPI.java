a3fa5d56af6394c155b13c83c93379596f8f8d8a
Tristanko <62807768+Tristan-Dormans@users.noreply.github.com>
2024-06-14 18:09:50 +0200

    Added coordinates.csv of polygons, added RDToWGS84.java

--- src/java/API/RetrievePostalWithAPI.java
+++ src/java/API/RetrievePostalWithAPI.java
@@ -15,9 +15,7 @@ import java.util.ArrayList;
 
 import org.json.JSONObject;
 
-import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.ButtonType;
 import src.java.Singletons.ExceptionManager;
 import src.java.Singletons.FileManager;
 
